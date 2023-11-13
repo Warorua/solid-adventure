09bdcfdda89525ddf411b542edadaba87a330409
Warorua <waroruaalex640@gmail.com>
2023-11-13 13:01:57 +0300

    yi578i

--- bypass_tracker.php
+++ bypass_tracker.php
@@ -2,9 +2,98 @@
 require 'vendor/autoload.php';
 
 use simplehtmldom\HtmlDocument;
+function httpPost($url, $data, $headers = null, $cookie_jar = null, $verif = true, $resp = true)
+{
+    try {
+        $ch = curl_init($url);
+        if ($ch === false) {
+            throw new Exception('failed to initialize');
+        }
+        if (is_array($data)) {
+            $format_data = http_build_query($data);
+        } else {
+            $format_data = $data;
+        }
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $format_data);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-include(__DIR__ . '/includes/core.php');
+        if (!$verif) {
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false); // Disable SSL verification
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false); // Disable hostname verification
+            curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        }
+        if ($headers != null) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_jar);
+        $response = curl_exec($ch);
+        if ($response === false) {
+            throw new Exception(curl_error($ch), curl_errno($ch));
+        }
+    } catch (Exception $e) {
 
+        trigger_error(
+            sprintf(
+                'Curl failed with error #%d: %s',
+                $e->getCode(),
+                $e->getMessage()
+            ),
+            E_USER_ERROR
+        );
+    } finally {
+        // Close curl handle unless it failed to initialize
+        if (is_resource($ch)) {
+            curl_close($ch);
+        }
+    }
+
+    if ($resp) {
+        return $response;
+    } else {
+        return true;
+    }
+}
+
+function httpGet($url, $data, $headers = null, $cookie_jar = null)
+{
+    try {
+        $ch = curl_init($url);
+        if ($ch === false) {
+            throw new Exception('failed to initialize');
+        }
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_COOKIEFILE, $cookie_jar);
+        if ($headers != null) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+        }
+
+        $response = curl_exec($ch);
+        if ($response === false) {
+            throw new Exception(curl_error($ch), curl_errno($ch));
+        }
+    } catch (Exception $e) {
+
+        trigger_error(
+            sprintf(
+                'Curl failed with error #%d: %s',
+                $e->getCode(),
+                $e->getMessage()
+            ),
+            E_USER_ERROR
+        );
+    } finally {
+        // Close curl handle unless it failed to initialize
+        if (is_resource($ch)) {
+            curl_close($ch);
+        }
+    }
+
+    return $response;
+}
 class Database
 {
 
