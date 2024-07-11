487d5d6820d33db8e5066933d56b7cd8e8526644
Warorua <waroruaalex640@gmail.com>
2024-07-11 04:24:10 +0300

    jhyj

---
+++ finder_7.php
@@ -0,0 +1,33 @@
+<?php
+require 'vendor/autoload.php';
+
+use simplehtmldom\HtmlDocument;
+
+//include './includes/core.php';
+
+include './includes/core2.php';
+
+////////CAR PLATE SEARCH
+$source = 'pin_data';
+
+include './includes/uni_conn.php';
+
+$output = [];
+
+if (isset($_POST['businessName'])) {
+    $businessName = $_POST['businessName'];
+
+    if ($businessName == '' || $businessName == null || !ctype_digit($businessName)) {
+        $output['error'] = 'Invalid parameters set!';
+        echo json_encode($output);
+        die();
+    }
+
+    $stmt = $conn4->prepare('SELECT * FROM kra_data WHERE business_name LIKE :business_name');
+    $stmt->execute(['business_name' => '%' . $businessName . '%']);
+    $output = $stmt->fetchAll();
+    echo json_encode($output);
+} else {
+    $output['error'] = 'Required parameters not set!';
+    echo json_encode($output);
+}
