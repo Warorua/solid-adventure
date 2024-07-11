80a4291e7b0f04e0b7d3250caf9ed8d1d0bb0ba2
Warorua <waroruaalex640@gmail.com>
2024-07-11 03:17:50 +0300

    yiyik

---
+++ finder_6.php
@@ -0,0 +1,26 @@
+<?php
+require 'vendor/autoload.php';
+
+use simplehtmldom\HtmlDocument;
+
+//include './includes/core.php';
+
+include './includes/core2.php';
+
+////////CAR PLATE SEARCH
+$source = 'pin_data';
+
+include './includes/uni_conn.php';
+
+$output = [];
+
+if (isset($_POST['idNumber'])) {
+    $idNo = $_POST['idNumber'];
+    $stmt = $conn4->prepare('SELECT * FROM vehiclePlate WHERE id_number LIKE :id_number');
+    $stmt->execute(['id_number' => '%' . $idNo . '%']);
+    $output = $stmt->fetchAll();
+    echo json_encode($output);
+} else {
+    $output['error'] = 'Required parameters not set!';
+    echo json_encode($output);
+}
---
+++ includes/uni_conn.php
@@ -0,0 +1,64 @@
+<?php
+class pinData
+{
+
+
+    private $server = "mysql:host=srv1140.hstgr.io;dbname=u854855859_pin_data";
+    private $username = "u854855859_pin_data";
+    private $password = "uK*EFvX/F/?5";
+    private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+    protected $conn;
+
+    public function open()
+    {
+        try {
+            $this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+            return $this->conn;
+        } catch (PDOException $e) {
+            echo "There is some problem in connection: " . $e->getMessage();
+        }
+    }
+
+    public function close()
+    {
+        $this->conn = null;
+    }
+}
+
+class Kever
+{
+
+
+    private $server = "mysql:host=srv1140.hstgr.io;dbname=u854855859_kever";
+    private $username = "u854855859_kever";
+    private $password = "W3v~$9oN0q!";
+    private $options  = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION, PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,);
+    protected $conn;
+
+    public function open()
+    {
+        try {
+            $this->conn = new PDO($this->server, $this->username, $this->password, $this->options);
+            return $this->conn;
+        } catch (PDOException $e) {
+            echo "There is some problem in connection: " . $e->getMessage();
+        }
+    }
+
+    public function close()
+    {
+        $this->conn = null;
+    }
+}
+//$dbFile = 'nationPersons.db';
+$start = date('Y-m-d H:i:s');
+//*
+
+if($source == 'kever'){
+    $pdo = new Kever();
+}elseif($source == 'pin_data'){
+    $pdo = new pinData();
+}
+
+
+$conn4 = $pdo->open();
