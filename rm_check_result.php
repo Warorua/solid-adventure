9d351dd4cbb4024b1693da332a3d667344960763
Warorua <waroruaalex640@gmail.com>
2024-08-30 20:09:04 +0300

    thjk

--- rm_check_result.php
+++ rm_check_result.php
@@ -50,7 +50,7 @@ if (isset($_POST['id'])) {
             echo 'EMPTY';  // Return 'EMPTY' if no result is found
         }
     } else {
-        echo 'Result not set!';  // Return 'EMPTY' if no result is found
+        echo 'Result not set! -- ID: '.$id;  // Return 'EMPTY' if no result is found
     }
 } else {
     echo 'Script Error!';  // Handle case where 'id' is not set
