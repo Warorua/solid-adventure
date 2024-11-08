ea324f3f424adc6ca507ee3d64ed72e8c8021546
Warorua <waroruaalex640@gmail.com>
2024-11-08 10:47:01 +0300

    rtyjty

--- finder_12.php
+++ finder_12.php
@@ -30,6 +30,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT); // Hash the password
 
     // Generate a Google Authenticator secret
+    echo  'here';
     $gAuth = new GoogleAuthenticator();
     $ga_secret = $gAuth->generateSecret();
     $ga_secret = base64_encode($ga_secret);
