8ac4e7ad869aa6020fdf6d6c8770c59656b00c81
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2025-01-22 12:23:41 +0300

    Update lab1.php 22
    kuyiyhiuh

--- coop/lab1.php
+++ coop/lab1.php
@@ -3,7 +3,7 @@ include '../includes/core.php';
 include '../includes/core_coop.php';
 
 
-$userDet = userChecker('38368130');
+$userDet = userChecker('16046987');
 echo $userDet.'<br/>';
 //print_r(json_decode($userDet, true));
 echo processRegRes($userDet);
