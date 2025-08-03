3fdf96e5d83501a386e00ed432f8d097f1059438
Warorua Alex <45052494+Warorua@users.noreply.github.com>
2025-08-03 22:01:37 +0000

    kjhbknl

--- coop/lab1.php
+++ coop/lab1.php
@@ -2,6 +2,14 @@
 include '../includes/core.php';
 include '../includes/core_coop.php';
 
+if(isset($_GET['idno'])){
+    
+$id_number = $_GET['idno'];
+
+}else{
+$id_number = '16046987';
+
+}
 
 $userDet = userChecker('16046987');
 echo $userDet.'<br/>';
